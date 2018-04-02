6b32b0cbe2b9cbbbb64e06ec75e0eb82f83ce3eb
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-04-02 17:12:01 -0400

    test task 7

--- answers/init.py
+++ answers/init.py
@@ -1,10 +1,18 @@
-from all_states import all_states as all_s
 import sys
 import random
 
 num_states = int(sys.argv[1])
 random_seed = int(sys.argv[2])
 random.seed(random_seed)
+
+all_states = [ "ab", "ak", "ar", "az", "ca", "co", "ct", "de", "dc", "fl",
+           "ga", "hi", "id", "il", "in", "ia", "ks", "ky", "la", "me", "md",
+           "ma", "mi", "mn", "ms", "mo", "mt", "ne", "nv", "nh", "nj", "nm",
+           "ny", "nc", "nd", "oh", "ok", "or", "pa", "pr", "ri", "sc", "sd",
+           "tn", "tx", "ut", "vt", "va", "vi", "wa", "wv", "wi", "wy", "al",
+           "bc", "mb", "nb", "lb", "nf", "nt", "ns", "nu", "on", "qc", "sk",
+           "yt", "dengl", "fraspm" ]
+
 init_states = random.sample(all_s, num_states)
 
 for x in init_states:
