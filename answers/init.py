d404d154cc03f2cc6bd290eb82e90b7a3a51864d
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-04-02 16:47:27 -0400

    test task 7

--- answers/init.py
+++ answers/init.py
@@ -2,9 +2,8 @@ from all_states import all_states as all_s
 import sys
 import random
 
-file_name = sys.argv[1]
-num_states = int(sys.argv[2])
-seed = int(sys.argv[3])
+num_states = int(sys.argv[1])
+seed = int(sys.argv[2])
 
 random.seed(seed)
 init_states = random.sample(all_s, num_states)
