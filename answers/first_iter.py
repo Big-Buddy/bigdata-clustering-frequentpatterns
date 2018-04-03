0bd0b18c765785b1dc3f304a6496151f939f8985
Big-Buddy <a.pelletier0x7c9@gmail.com>
2018-04-03 12:05:35 -0400

    test task 8

--- answers/first_iter.py
+++ answers/first_iter.py
@@ -21,7 +21,7 @@ def distance_combine(data):
 		el00 = data[dict_keys[1]]
 		el01 = dict_keys[0]
 		el1 = data[dict_keys[0]]
-	return (el00, (el01, [el1]))
+	return (el00, (el01, list(el1)))
 
 def euclid_sqr(a,b):
 	distance = 0
